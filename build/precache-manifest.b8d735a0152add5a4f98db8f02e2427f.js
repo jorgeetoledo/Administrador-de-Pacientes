d834972fb5adbe4c8f76c59925bc7ddcb42791dd
Eduardo Toledo <68624752+jorgeetoledo@users.noreply.github.com>
2020-08-25 21:25:21 -0300

    Aplicacion de Citas

---
+++ build/precache-manifest.b8d735a0152add5a4f98db8f02e2427f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20dfaca830cfef744580754b73493e3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "938abba8257bcb9504c8",
+    "url": "/static/css/main.2fb46aa8.chunk.css"
+  },
+  {
+    "revision": "f323d1d3406560d47342",
+    "url": "/static/js/2.19e7dc1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.19e7dc1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "938abba8257bcb9504c8",
+    "url": "/static/js/main.795722f7.chunk.js"
+  },
+  {
+    "revision": "9078284b3c5a7cc4441c",
+    "url": "/static/js/runtime-main.3c7fdb96.js"
+  }
+]);
